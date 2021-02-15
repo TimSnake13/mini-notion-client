7771f436228fe52bf9ffd90de7e9952d8a0ab374
GoG <forsoapfc@gmail.com>
2021-02-15 10:28:51 +0800

    Moving files & start aws amplify dev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import Amplify, { Auth } from "aws-amplify";
+import awsconfig from "../src/aws-exports";
+Amplify.configure(awsconfig);
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
